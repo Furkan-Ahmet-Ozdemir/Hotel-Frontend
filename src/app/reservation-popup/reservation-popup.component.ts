5d12da03d36f80b9c6c1b97db55e7810516f3f17
MithatBerhan <104715598+MithatBerhan@users.noreply.github.com>
2023-07-19 16:44:56 +0300

    Filter revision

--- src/app/reservation-popup/reservation-popup.component.ts
+++ src/app/reservation-popup/reservation-popup.component.ts
@@ -11,12 +11,19 @@ export class ReservationPopupComponent {
     
   numberOfGuests: number = 1; // Varsayılan değer
   guestOptions: number[] = [0, 1, 2, 3, 4];
-  onSubmit(formValues: any) {
-    const startDate = formValues.startDate;
-    const endDate = formValues.endDate;
-    const numberOfGuests = formValues.numberOfGuests;
+  onSubmit() {
+
+    if (this.startDate && this.endDate) {
+      if (this.startDate < new Date()) {
+        alert('Başlangıç tarihi geçmiş bir tarih olamaz.');
+      } else if (this.endDate < this.startDate) {
+        console.log('Bitiş tarihi başlangıç tarihinden önce olamaz.');
+      } else {
+        console.log('Rezervasyon işlemi başarılı.');
 
     // Oda tipi filtreleme işlemini gerçekleştir
     // veya filtreleme işlemi için başka bir fonksiyonu çağır
   }
+}
+}
 }
